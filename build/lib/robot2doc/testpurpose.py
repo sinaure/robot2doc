2090516998457f028533f1426f635c3a98f4c935
aureliano sinatra <sinaure@gmail.com>
2019-07-12 12:09:19 +0200

    added commit message to script

--- build/lib/robot2doc/testpurpose.py
+++ build/lib/robot2doc/testpurpose.py
@@ -34,11 +34,12 @@ class TP():
     def __str__(self):
         return str(self.lines)
 
-    def add_to_spec(self, spec : testspec.TestSpec, testbehaviour: str, robot_file : str, commit_id=None):
+    def add_to_spec(self, spec : testspec.TestSpec, testbehaviour: str, robot_file : str, commit_id : str):
         '''
         Given a Test Spec, executes the addition of the this TP in the document.
         '''
         spec.add_tp(self.tp_fields, testbehaviour)
+        print("adding commit id: "+commit_id)
         if commit_id != None:
             spec.add_commit_url(commit_id, robot_file)
         else:    
---
+++ execOverFolder.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+EXCLUDE=""
+if [ "$1" != "" ]; then
+	DIR=$1
+	if [ "$2" != "" ]; then
+		EXCLUDE=$2
+	else
+		echo "No configured exclusions"
+	fi
+	if [ "$3" != "" ]; then
+		COMMIT=$3
+	else
+		echo "No configured commit id"
+	fi
+		
+	for file in $DIR/*.robot; do
+		filename=$(basename "$file")
+		if [[ $filename != *"$EXCLUDE"* ]]; then
+			python robot2doc/main.py $file "${filename%.*}.docx" $COMMIT
+		else
+			echo "excluded file: $filename"	
+		fi
+	done
+else
+    echo "Enter a directory"
+fi
+
